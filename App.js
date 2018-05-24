6075d6d104c4aed3eb2a7be4a6f7fc7ca454cba2
Robert Tate <robertjosephtate@gmail.com>
2018-05-24 09:31:18 -0500

    updated app.js

--- App.js
+++ App.js
@@ -11,27 +11,10 @@ export default class App extends Component {
   constructor(props) {
     super(props);
 
-<<<<<<< HEAD
-  render() {
-    if (isLoggedIn()) {
-      return (
-        <MapScreen />
-      )
-    } else {
-      return (
-        <View>
-          <SplashScreen />
-          <MapScreen />
-        </View>
-      )
-=======
     this.state = {
       signedIn: false,
->>>>>>> 6928fa9793dd87df6ac44f5631c0170f3cda473e
     }
   }
-}
-
 
   componentDidMount() {
     if (isLoggedIn()) {
@@ -39,16 +22,11 @@ export default class App extends Component {
     }
   }
 
-<<<<<<< HEAD
-
-
-=======
   render() {
     
     const Layout = createRootNavigator(this.state.signedIn);
     return <Layout />
   }
 }
->>>>>>> 6928fa9793dd87df6ac44f5631c0170f3cda473e
 
 
