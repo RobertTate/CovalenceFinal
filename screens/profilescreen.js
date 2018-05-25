40f05cd658ee9d41353511a114d91287a9803fc8
Justin Head <justin_head@ymail.com>
2018-05-25 15:23:54 -0500

    begun work on profile screen

--- screens/profilescreen.js
+++ screens/profilescreen.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation';
+import { View } from 'react-native';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 
 
 export default class ProfileScreen extends Component {
-
+    static navigationOptions = {
+        title: "Profile",
+        headerStyle: {backgroundColor: '#4054b2'}
+    }
 
     render() {
         return (
             <Container>
-                <Content>
-                    <Text>This is the profile screen</Text>
+                <Content contentContainerStyle={{ flex: 1,
+                                                  flexDirection: 'column',
+                                                  justifyContent: 'space-between'}}>
+                    <View>
+                        <Button style={{ alignSelf: 'start' }} onPress={() => this.props.navigation.navigate('Settings')}>
+                        <Icon name="ios-settings-outline" />
+                        </Button>
+                        <Button style={{ alignSelf: 'end' }} onPress={() => this.props.navigation.navigate('SignedOut')}>
+                            <Text>Log Out</Text>
+                        </Button>
+                    </View>
                 </Content>
             </Container>
         )
